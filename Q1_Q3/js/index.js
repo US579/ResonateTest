4b89d63fcc8689aac222503896b3d94dccf41bee
us579 <wzus579@gmail.com>
2019-11-09 23:35:45 +1100

    add date by using base64

--- Q1_Q3/js/index.js
+++ Q1_Q3/js/index.js
@@ -1,18 +1,4 @@
 // TODO: Modify this function
-// function string10to64(number) {
-//   //  console.log(number)
-//     var chars = '0123456789abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ-~'.split('')
-//     radix = chars.length
-//     qutient = +number
-//     arr = [];
-//   do {
-//     mod = qutient % radix;
-//     qutient = (qutient - mod) / radix;
-//     arr.unshift(chars[mod]);
-//   } while (qutient);
-//   return arr.join('');
-// }
-
 function string10to64(num) {
   var order = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-";
   var base = order.length;
@@ -25,19 +11,6 @@ function string10to64(num) {
   }
   return str;
 }
-
-// function string64to10(number_code) {
-//   var chars = '0123456789abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ-~',
-//     radix = chars.length,
-//     number_code = String(number_code),
-//     len = number_code.length,
-//     i = 0,
-//     origin_number = 0;
-//   while (i < len) {
-//     origin_number += Math.pow(radix, i++) * chars.indexOf(number_code.charAt(len - i) || 0);
-//   }
-//   return origin_number;
-// }
 
 
 function string64to10(str) {
