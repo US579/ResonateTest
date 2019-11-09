3c2237bd1d94f2120b8cc58c9b771520023cfbef
us579 <wzus579@gmail.com>
2019-11-09 23:36:26 +1100

    add date by using base64

--- Q1_Q3/js/index.js
+++ Q1_Q3/js/index.js
@@ -30,9 +30,6 @@ function timetrans(date) {
   var Y = date.getFullYear().toString().substring(2,4);
   var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1);
   var D = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate());
-  // var h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
-  // var m = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':';
-  // var s = (date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds());
   return Y + M + D ;
 }
 
