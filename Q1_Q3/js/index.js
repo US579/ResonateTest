3db239db5aebfab12a04028f0dc2db239309d8b1
us579 <wzus579@gmail.com>
2019-11-10 13:00:27 +1100

    add date by using base64

--- Q1_Q3/js/index.js
+++ Q1_Q3/js/index.js
@@ -12,7 +12,6 @@ function string10to64(num) {
   return str;
 }
 
-
 function string64to10(str) {
   var order = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-";
   var base = order.length;
@@ -27,12 +26,11 @@ function string64to10(str) {
 }
 
 function timetrans(date) {
-  var Y = date.getFullYear().toString().substring(2,4);
+  var Y = date.getFullYear().toString();
   var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1);
   var D = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate());
   return Y + M + D ;
 }
-
 
 function repeat(str, num) {
   if (num <= 0) {
@@ -46,32 +44,7 @@ function repeat(str, num) {
   }
 }
 
-
-// console.log(date)
-// console.log(myDate.getDay().toString())
-// console.log(string10to64(date))
-
-function generateShortCode(storeId, transactionId) {
-  // Logic goes here
-  var sId = (storeId.toString().length < 3 ? repeat("0",3-storeId.toString().length) + storeId.toString() : storeId.toString());
-  var tId = (transactionId.toString().length < 4 ? repeat("0", 4 - transactionId.toString().length) + transactionId.toString() : transactionId.toString())
-  var myDate = new Date();
-  var date = timetrans(myDate)
-  // console.log(sId)
-  // console.log(tId)
-  // console.log(date)
-  id = date + sId + tId
-  // console.log(id)
-  ans = string10to64(id)
-  console.log(ans)
-  return ans
-}
-
-// var a = generateShortCode(175,123)
-// decodeShortCode(a);
-
-
-function repeatRecover(string){
+function repeatRecover(string) {
   for (var i = 0; i < string.length; i++) {
     if (string[i] === "0") {
       continue
@@ -82,20 +55,26 @@ function repeatRecover(string){
   return parseInt(string.substring(index))
 }
 
+function generateShortCode(storeId, transactionId) {
+  // Logic goes here
+  var sId = (storeId.toString().length < 3 ? repeat("0",3-storeId.toString().length) + storeId.toString() : storeId.toString());
+  var tId = (transactionId.toString().length < 4 ? repeat("0", 4 - transactionId.toString().length) + transactionId.toString() : transactionId.toString())
+  var myDate = new Date();
+  var date = timetrans(myDate)
+  id = date + sId + tId
+  ans = string10to64(id)
+  return ans
+}
+
 // TODO: Modify this function
 function decodeShortCode(shortCode) {
   // Logic goes here
   shortCode = string64to10(shortCode).toString()
-  // console.log(shortCode)
-  var date = shortCode.substring(0, 6);
-  var sid = shortCode.substring(6, 9);
-  var tid = shortCode.substring(9, 13);
-  // console.log(date.substring(0, 6));
-  // console.log(date.substring(6,9))
+  var date = shortCode.substring(0, 8);
+  var sid = shortCode.substring(8, 11);
+  var tid = shortCode.substring(11, 15);
   var nDate = new Date();
-  // nDate.setFullYear('2018',parseInt(),12)
-  nDate.setFullYear('20' + date.substring(0, 2), repeatRecover(date.substring(2, 4))-1, repeatRecover(date.substring(4, 6)))
-  // console.log(nDate)
+  nDate.setFullYear( date.substring(0, 4), repeatRecover(date.substring(4, 6))-1, repeatRecover(date.substring(6, 8)))
   return {
     storeId: repeatRecover(sid), // store id goes here,
     shopDate: nDate, // the date the customer shopped,
